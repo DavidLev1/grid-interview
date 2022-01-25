15b03aa68210b2b328e7c20120bb043df7c4688a
DavidLev1 <DavidLev1@example.com>
2022-01-25 18:00:23 +0200

    aaa

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import Grid from './Grid';
 import movies from './data_a';
 import pokemon from './data_b';
 
+
 const Trailer = ({ data }) => (
-  <a href={data.url}
-     target="_blank"
-     rel="noopener noreferrer">
-    trailer...
-  </a>
+  // <a href={data.url}
+  //    target="_blank"
+  //    rel="noopener noreferrer">
+  //   trailer...
+  // </a>
+  <span>Trailer</span>
 );
 
 const PokemonName = ({ data }) => <span style={{ textTransform: 'capitalize' }}>{data}</span>;
+
 
 const moviesConfig = [
   {
@@ -26,11 +29,12 @@ const moviesConfig = [
     title: 'rating',
     field: 'imdbRating',
   },
- // {
- //   title: 'trailer',
- //   field: 'Trailer',
- //   component: Trailer
- // }
+  
+ {
+   title: 'trailer',
+   field: 'Trailer',
+   component: <Trailer/>
+ }
 ];
 
 const pokemonConfig = [
@@ -38,17 +42,18 @@ const pokemonConfig = [
     title: 'pokedex #',
     field: 'number'
   },
-  // {
-  //   title: 'name',
-  //   field: 'name',
-  //   component: PokemonName
-  // },
+  {
+    title: 'name',
+    field: 'name',
+    component: <PokemonName data={<span>PokemonName</span>}/>
+  },
 ];
 
 const App = () => (
   <div>
     <h2>Movies</h2>
     <Grid config={moviesConfig} data={movies} />
+    <br></br>
 
     <h2>Pokemon</h2>
     <Grid config={pokemonConfig} data={pokemon} />
--- src/Grid.js
+++ src/Grid.js
@@ -1,24 +1,56 @@
 import React from 'react';
 
-const Grid = ({ config, data }) => (
-  <table>
-    <thead>
-    <tr>
-      <th>Col 1</th>
-      <th>Col 2</th>
-    </tr>
-    </thead>
-    <tbody>
-    <tr>
-      <td>Data 1</td>
-      <td>Data 2</td>
-    </tr>
-    <tr>
-      <td>Data 1</td>
-      <td>Data 2</td>
-    </tr>
-    </tbody>
-  </table>
-);
+
+const Grid = ({ config, data }) => {
+  let index = 0;
+
+  const elements = data.map(item => {
+    let title;
+    let field;
+    let component;
+    let elemRows = [];
+    
+    config.forEach(i => {
+      title = i.title;
+
+      field = item[i.field];
+      if(typeof field === 'object') field = JSON.stringify(field);
+
+      if(i.component) component = i.component;
+      elemRows.push({ title, field, component});
+    })
+
+    const element = elemRows.map(row => {
+      index++;
+    
+      return (
+        <tr key={index}>
+          <td>{row.title}</td>
+          <td>{row.field}</td>
+          <td>{row.component}</td>
+        </tr> 
+      )  
+    })
+
+    return (
+      <>{element}</> 
+    )
+  });
+
+  return (
+    <table>
+      <thead>
+      <tr>
+        <th>Title</th>
+        <th>Field</th>
+        <th>Component</th>
+      </tr>
+      </thead>
+
+      <tbody>{elements}</tbody>
+    </table>
+    
+  )
+};
 
 export default Grid;
